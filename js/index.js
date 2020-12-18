8eb571d408e3c89f89e0789cda3206dad79b423a
mymarch <75958254+mymarch@users.noreply.github.com>
2020-12-18 14:09:18 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -200,5 +200,14 @@ const handlerClickSendEmailBtn = (name, email, res) => {
 };
 
 const handlerClickCloseBtn = () => {
-    window.close();
-}
+    var nvua = navigator.userAgent;
+    if (nvua.indexOf("MSIE") >= 0) {
+        if (nvua.indexOf("MSIE 5.0") == -1) {
+            top.opener = "";
+        }
+    } else if (nvua.indexOf("Gecko") >= 0) {
+        top.name = "CLOSE_WINDOW";
+        wid = window.open("", "CLOSE_WINDOW");
+    }
+    top.close();
+};
